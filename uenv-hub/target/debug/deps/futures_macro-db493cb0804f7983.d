86f69e2364b527fc76e208802beee15fd7285c41
liu <liuruixin.work@foxmail.com>
2026-05-29 12:43:55 +0800

    feat(uenv-hub): implement UEnvHub registry (HTTP REST API + SQLite) with types/core/server/client crates, CLI, auth/RBAC, validation, templates, docs and deploy artifacts

--- uenv-hub/target/debug/deps/futures_macro-db493cb0804f7983.d
+++
@@ -1,9 +0,0 @@
-/home/cid/Sources/UENV/uenv/uenv-hub/target/debug/deps/futures_macro-db493cb0804f7983.d: /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/lib.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/executor.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/join.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/select.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/stream_select.rs
-
-/home/cid/Sources/UENV/uenv/uenv-hub/target/debug/deps/libfutures_macro-db493cb0804f7983.so: /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/lib.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/executor.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/join.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/select.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/stream_select.rs
-
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/lib.rs:
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/executor.rs:
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/join.rs:
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/select.rs:
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-macro-0.3.32/src/stream_select.rs:
--- uenv-hub/target/debug/deps/snafu_derive-3ee396e419ddcc46.d
+++
@@ -1,8 +0,0 @@
-/home/cid/Sources/UENV/uenv/uenv-hub/target/debug/deps/snafu_derive-3ee396e419ddcc46.d: /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/lib.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/parse.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/shared.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/report.rs
-
-/home/cid/Sources/UENV/uenv/uenv-hub/target/debug/deps/libsnafu_derive-3ee396e419ddcc46.so: /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/lib.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/parse.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/shared.rs /home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/report.rs
-
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/lib.rs:
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/parse.rs:
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/shared.rs:
-/home/cid/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/snafu-derive-0.8.9/src/report.rs:
